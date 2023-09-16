0d379765913b56a7e4a4f9aba7a265a74737c358
Xavier Quintanilla <xquintan@ualberta.ca>
2023-09-16 17:23:51 -0600

    comment intention of methods in controllers

--- back-end/controllers/user-controller.js
+++ back-end/controllers/user-controller.js
@@ -1,24 +1,29 @@
 
 
 const getUserData = (req, res, next) => {
+    //retrieve a user info from database. (e.g. when they want to see their profile)'
     res.send('In this request we will retrieve a user info from database. (e.g. when they want to see their profile)');
     next();
 }
 
 const createUser = (req, res, next) => {
+    //send the registered user data to the database
+    const {f_name, l_name, b_day, email, password} = req.body
+
     res.send('In this request we will send the registered user data to the database')
 }
 
 const validateUser = (req, res, next) => {
-    // check if user matches witt db
+    // check if user matches with db
     const {email, password} = req.body
-    
+
 
 
 }
 
 const modifyUser = (req, res, next) => {
 //update the user information in the database
+
 }
 
 
