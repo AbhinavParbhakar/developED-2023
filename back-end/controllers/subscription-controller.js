ab6cdb02208222310039369b189bdffa5601e81c
Xavier Quintanilla <xquintan@ualberta.ca>
2023-09-16 16:16:58 -0600

    add method descriptions for controllers

---
+++ back-end/controllers/subscription-controller.js
@@ -0,0 +1,5 @@
+const getSubscriptions  = (req, res, next) => {
+    // get all the supscriptions associated with a certain user
+
+}
+
--- back-end/controllers/user-controller.js
+++ back-end/controllers/user-controller.js
@@ -5,4 +5,25 @@ const getUserData = (req, res, next) => {
     next();
 }
 
+const createUser = (req, res, next) => {
+    res.send('In this request we will send the registered user data to the database')
+}
+
+const validateUser = (req, res, next) => {
+    // check if user matches witt db
+    const {email, password} = req.body
+
+
+}
+
+const modifyUser = (req, res, next) => {
+//update the user information in the database
+}
+
+
+
+
+
 exports.getUserData = getUserData;
+exports.createUser = createUser;
+
