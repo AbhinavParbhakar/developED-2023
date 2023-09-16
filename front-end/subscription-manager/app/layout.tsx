9959e787ab59e2d3efa1344645ee1aa976ca1795
Diego Arana <106112103+zyzCodes@users.noreply.github.com>
2023-09-16 13:13:12 -0600

    fornt-end first commit

--- front-end/subscription-manager/app/layout.tsx
+++ front-end/subscription-manager/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Navbar from '../components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +16,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <div>
+      <Navbar/>
       <body className={inter.className}>{children}</body>
-    </html>
+    </div>
   )
 }
