b9fcc50f30f8bc8d2095cfeb876d812c24da1535
Diego Arana <106112103+zyzCodes@users.noreply.github.com>
2023-09-16 13:39:47 -0600

    Edit readme file

--- front-end/subscription-manager/app/page.tsx
+++ front-end/subscription-manager/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Welcome User</h1>
+            <h1 className="text-5xl font-bold">Welcome Back User</h1>
             <p className="py-6">Never forget your subscriptions again, only pay for what you want</p>
             <button className="btn btn-primary">Add subscription</button>
           </div>
