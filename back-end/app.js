e78368478ddc15cc3b0dcb6fa8f1c08e50de1dc7
gabofv <gabriel.felipe.008@gmail.com>
2023-09-16 16:27:53 -0600

    add app.use for /user route

--- back-end/app.js
+++ back-end/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const userRoutes = require('./routes/user-routes');
+
 const app = express();
 
 const LISTEN_PORT = 8080;
+
+app.use('/user', userRoutes);
 
 app.get('/', (req, res, next) => {
     res.send('Backend working on root dir');
