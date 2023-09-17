a40331e09f2cb87e46b7abfbd42b855b7ed88c82
Diego Arana <106112103+zyzCodes@users.noreply.github.com>
2023-09-17 15:08:38 -0600

    edited the layout

--- front-end/subscription-manager/app/page.tsx
+++ front-end/subscription-manager/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <div className="hero-content text-center">
           <div className="max-w-md">
             <h1 className="text-5xl font-bold">Welcome Back User</h1>
-            <p className="py-6">Never forget your subscriptions again! Only pay for what you want!</p>
+            <p className="text-base mt-5">Never forget about your subscriptions again!</p>
             <Link href="/subscription/new" className="btn btn-primary mt-4 mb-4">Add subscription</Link>
             <div className={styles.subs}>
               <div className={styles.subsTitle}>Your Upcoming subscriptions:</div>
