7a833c83c85abbba0bd4ec279376e6fdcb308251
Xavier Quintanilla <xquintan@ualberta.ca>
2023-09-16 19:49:01 -0600

    create users modification

--- back-end/models/user-model.js
+++ back-end/models/user-model.js
@@ -3,6 +3,7 @@ const sql = require('./db');
 
 const User = function () {};
 
+//Check scope of this function (can be for user login)
 User.getById = (id, handler) => {
     sql.query(
         'SELECT full_name FROM users_pretty WHERE id = ?',
@@ -18,6 +19,7 @@ User.getById = (id, handler) => {
         });    
 }
 
+// creates a new user in the table users
 User.create = (id, newUser, handler) => {
     sql.query(
         'INSERT INTO TABLE users (id, f_name, l_name, email, passwd, b_day) VALUES (?, ?, ?, ?, ?)',
