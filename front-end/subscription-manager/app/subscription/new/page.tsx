1ccaaf94cdd4df7e64b1f907e21ab06933b3b739
Diego Arana <106112103+zyzCodes@users.noreply.github.com>
2023-09-17 13:00:52 -0600

    testing form submission

--- front-end/subscription-manager/app/subscription/new/page.tsx
+++ front-end/subscription-manager/app/subscription/new/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import { redirect } from 'next/navigation'
-import React, { ReactEventHandler, useState } from 'react';
+import React, { useState } from 'react';
 
 function AddSubscription() {
   // Define the subscription state outside of the component
