83000a32600a958601a85ac94c10850273c356c5
Abhinav parbhakar <abhinav.parbhakar302@gmail.com>
2023-12-19 00:02:37 -0700

    pushing more changes

---
+++ front-end/subscription-manager/app/privacy/page.tsx
@@ -0,0 +1,32 @@
+export default function Privacy(){
+    return (
+        <div className="max-w-screen text-center items-center">
+    <div className="flex flex-col mt-4 justify-items-center items-center max-w-xl">
+        <h1 className="text-3xl font-bold">Privacy Statement</h1><br/>
+At Submanager, we are committed to protecting the privacy and confidentiality of our users' information. This Data Privacy Statement outlines how we collect, use, and safeguard the data provided by our users, specifically focusing on email addresses.
+
+1. Collection of Email Addresses:
+Submanager may collect email addresses from users for the sole purpose of identifying and authenticating users on our platform. The email address serves as a unique identifier and facilitates secure access to Submanager's services.
+
+2. Use of Email Addresses:
+The email addresses provided to Submanager will be used exclusively for the purpose of user identification and communication related to our services. We may use the email address to send important notifications, updates, or information related to Submanager features and enhancements.
+
+3. No Sharing or Distribution:
+We want to reassure our users that Submanager is committed to maintaining the confidentiality of the information they entrust to us. We will not share, sell, rent, or distribute users' email addresses to third parties without explicit consent, except as required by law.
+
+4. Data Security Measures:
+Submanager employs robust security measures to protect the confidentiality and integrity of user data, including email addresses. We utilize encryption, access controls, and other industry-standard security practices to ensure that your information is safeguarded against unauthorized access or disclosure.
+
+5. User Control and Consent:
+Submanager acknowledges the importance of user control over personal information. Users have the right to update, correct, or delete their email addresses from our records. Additionally, users have the option to manage their communication preferences and can opt out of non-essential communications.
+
+6. Compliance with Applicable Laws:
+Submanager operates in compliance with applicable data protection and privacy laws. We continuously monitor and update our practices to align with evolving legal requirements to ensure the highest standards of data protection.
+
+7. Changes to the Privacy Statement:
+Submanager reserves the right to update or modify this Data Privacy Statement to reflect changes in our practices or legal requirements. Users will be notified of any significant changes, and continued use of our services constitutes acceptance of the revised privacy terms.
+
+By using Submanager's services, you agree to the terms outlined in this Data Privacy Statement. If you have any questions or concerns about our privacy practices, please contact us at [contact@submanager.com].
+    </div>
+    </div>)
+}
